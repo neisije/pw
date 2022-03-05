40d4bc8624da494f26d35b4d73deab4e38fd28d8
Jean-Christophe Neisius <Jean-Christophe.Neisius@ec.europa.eu>
2022-03-05 07:54:56 +0100

    onButtonClick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  useLetters : boolean = false;
+  useNumbers : boolean = false;
+  useSymbols : boolean = false;
+  pwlength = 8;
+
+  onUseLettersChange() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onUseNumbersChange() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onUseSymbolsChange() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onLengthChange(event: any) {
+    this.pwlength = event.target.value;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.useLetters) {
+      validChars += letters;
+    }
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.pwlength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
